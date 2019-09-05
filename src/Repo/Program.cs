446638d80e1d54957315c6912fb6b846fd3aeaf5
ihugya1 <50344364+ihugya1@users.noreply.github.com>
2019-09-05 14:56:53 -0600

    Closes #2

--- src/Repo/Program.cs
+++ src/Repo/Program.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net.Http; //for the httpclient class
+using static System.Console; // for using the static methods of the Console without having to reference the class name.
 namespace Repo
 {
+    
     class Program
     {
+        const string API_BASE = "https://www.gitignore.io/api/";
+        const string API_LIST_LINES = API_BASE + "list? format=lines";
         static void Main(string[] args)
         {
-            
+            var client = new HttpClient();
+            var response = client.GetAsync(API_LIST_LINES).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                WriteLine(content);
+                ReadLine();
+            }
         }
     }
 }
